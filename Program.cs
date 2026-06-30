3fe7dc762f1ec19fbeb9e3943263faaa52db2a5e
black <black@example.com>
2026-06-30 18:51:04 +0500

    fixes

--- Program.cs
+++ Program.cs
@@ -17,3 +17,23 @@ class Program
         SayHello();
     }
 }
+using System;
+
+class Program
+{
+    static void SayHello()
+    {
+        Console.WriteLine("hello everyone");
+    }
+
+    static void GreetBlack()
+    {
+        Console.WriteLine("hello black");
+    }
+
+    static void Main()
+    {
+        SayHello();
+        GreetBlack();
+    }
+}
